8fba86538fec8167514192d75c32fd1cc3e140d5
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-14 15:27:10 +0100

    add button position in section slide

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* 
+`
+<div class="box-img" >
+<img src="./img/01.webp" alt="">
+</div> 
+`
+*/
+
+const image = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+];
+for (let i = 0; i < image.length; i++){
+    
+}

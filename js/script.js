5a909cead6e9b246583c47a70b737bc692d26824
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-14 15:36:43 +0100

    console.log cicle for  js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 </div> 
 `
 */
-
+// SECTION IMAGE
 const image = [
     'img/01.webp',
     'img/02.webp',
@@ -13,6 +13,20 @@ const image = [
     'img/04.webp',
     'img/05.webp'
 ];
+
+
+// SECTION COSTANTI/VARIABILI
+
+const slide = document.querySelector('slide');
+let boxSlide = '';
+// CICLI
+
 for (let i = 0; i < image.length; i++){
-    
+    boxSlide += `
+    <div class="box-img" >
+    <img src="${image[i]}" alt="superhero-${i}}">
+    </div> 
+    `
 }
+console.log(boxSlide);
+
